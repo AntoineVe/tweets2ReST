6e4d4135dd1837c723d0262e669631824ffc1eaa
VAN ELSTRAETE Antoine <antoine@van-elstraete.net>
2017-03-05 23:57:00 +0100

    PEP-8 compliant !

--- tweets2rst.py
+++ tweets2rst.py
@@ -7,7 +7,6 @@ Code licence : WTFPL
 """
 
 import twitter
-import json
 from datetime import datetime
 from dateutil import tz
 import locale
@@ -21,40 +20,62 @@ import logging
 
 locale.setlocale(locale.LC_ALL, 'C')
 
+
 def get_tweets(token, token_key, con_secret, con_secret_key, twitter_name):
     try:
         articles = listdir('./content/SocialNetworks')
-        article = [article for article in articles if 'tweet' in article]
+        articles = [article for article in articles if 'tweet' in article]
         articles.sort()
         last_recorded_tweet_id = int(articles[-1][6:-4])
         logging.debug("last recoreded tweet : " + str(last_recorded_tweet_id))
     except:
         last_recorded_tweet_id = None
         logging.debug("No recorded tweets founded")
-    t = twitter.Twitter(auth=twitter.OAuth(token, token_key, con_secret, con_secret_key))
+    t = twitter.Twitter(
+            auth=twitter.OAuth(
+                token,
+                token_key,
+                con_secret,
+                con_secret_key))
     if last_recorded_tweet_id:
-        my_tweets = t.statuses.user_timeline(screen_name=twitter_name,count=600,since_id=last_recorded_tweet_id)
+        my_tweets = t.statuses.user_timeline(
+                screen_name=twitter_name,
+                count=600,
+                since_id=last_recorded_tweet_id)
     else:
-        my_tweets = t.statuses.user_timeline(screen_name=twitter_name,count=600)
+        my_tweets = t.statuses.user_timeline(
+                screen_name=twitter_name,
+                count=600)
     logging.debug(str(len(my_tweets)) + " tweets retrieved")
     return(my_tweets)
+
 
 def tweet2rest(tweets_json):
     """
     Use the JSON from twitter API to make one ReST article by tweet.
     """
     for tweet in tweets_json:
-        if not tweet['retweeted'] and tweet['in_reply_to_status_id_str'] == None  and tweet['text'][0] != '@' and not 'Instagram' in tweet['source']:
+        if (
+                not tweet['retweeted']
+                and tweet['in_reply_to_status_id_str'] is None
+                and tweet['text'][0] != '@'
+                and 'Instagram' not in tweet['source']):
             text = tweet['text']
             summary = text.split("\n")[0]
             data = "####################\n"
             data += tweet['id_str'] + "\n"
             data += "####################\n"
             data += "\n"
-            date = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S %z %Y")
-            data += ":date: " + date.astimezone(tz.gettz('Europe/Paris')).strftime("%Y-%m-%d %H:%M:%S") + "\n"
+            date = datetime.strptime(
+                    tweet['created_at'], "%a %b %d %H:%M:%S %z %Y")
+            date = date.astimezone(tz.gettz('Europe/Paris'))
+            data = date.strftime("%Y-%m-%d %H:%M:%S")
+            data += ":date: "
+            data += date + "\n"
             try:
-                location = str(tweet['place']['full_name']) + "," + str(tweet['coordinates']['coordinates'][1]) + "," + str(tweet['coordinates']['coordinates'][0])
+                location = str(tweet['place']['full_name'])
+                location += "," + str(tweet['coordinates']['coordinates'][1])
+                location += "," + str(tweet['coordinates']['coordinates'][0])
                 data += "location: " + location
             except:
                 pass
@@ -64,12 +85,17 @@ def tweet2rest(tweets_json):
                         data += ":tags: "
                         for tag in tweet['entities']['hashtags']:
                             data += tag['text'] + ", "
-                        data = data[:-2] # Remove last comma-space
+                        data = data[:-2]  # Remove last comma-space
                         data += "\n"
-                if "media" in tweet['entities'].keys() and tweet['entities']['media'][0]['type'] == "photo":
-                    data += ":image: {photo}../images/tweets/" + tweet['entities']['media'][0]['id_str'] + ".jpg\n"
-                    data += ":og_image: /images/tweets/" + tweet['entities']['media'][0]['id_str'] + ".jpg\n"
-                    img = urllib.request.urlopen(tweet['entities']['media'][0]['media_url']).read()
+                if (
+                        "media" in tweet['entities'].keys()
+                        and tweet['entities']['media'][0]['type'] == "photo"):
+                    data += ":image: {photo}../images/tweets/"
+                    data += tweet['entities']['media'][0]['id_str'] + ".jpg\n"
+                    data += ":og_image: /images/tweets/"
+                    data += tweet['entities']['media'][0]['id_str'] + ".jpg\n"
+                    img = urllib.request.urlopen(
+                            tweet['entities']['media'][0]['media_url']).read()
                     try:
                         stat("./content/images")
                     except:
@@ -77,27 +103,55 @@ def tweet2rest(tweets_json):
                     try:
                         stat("./content/images/tweets")
                     except:
-                         mkdir("./content/images/tweets")
-                    Image.open(io.BytesIO(img)).save("./content/images/tweets/" + tweet['entities']['media'][0]['id_str'] + ".jpg", quality=95, optimize=True)
-                    logging.debug("Image " + tweet['entities']['media'][0]['id_str'] + ".jpg saved")
+                        mkdir("./content/images/tweets")
+                    Image.open(io.BytesIO(img)).save(
+                            "./content/images/tweets/"
+                            + tweet['entities']['media'][0]['id_str']
+                            + ".jpg", quality=95, optimize=True)
+                    logging.debug(
+                            "Image "
+                            + tweet['entities']['media'][0]['id_str']
+                            + ".jpg saved")
                     for img in tweet['entities']['media']:
                         summary = summary.replace(img['url'], '')
                         text = text.replace(img['url'], '')
-                #TODO : Add gallery support for multiple photos in a tweet.
+                # TODO : Add gallery support for multiple photos in a tweet.
             for url in URLExtract().find_urls(text):
                 text = text.replace(url, "`"+url+" <"+url+">`_")
             text_2 = list()
             for word in text.split():
-                if word[0] == "@" or word[0:2] == ".@" :
-                    if word[-1].isalnum(): # Take care of non alphanum at the end, like comma or point.
-                        word = word.replace(word, '`' + word + ' <https://twitter.com/' + word[1:] + '>`_')
+                if word[0] == "@" or word[0:2] == ".@":
+                    # Take care of non alphanum at the end, like comma or point
+                    if word[-1].isalnum():
+                        word = word.replace(
+                                word, '`'
+                                + word
+                                + ' <https://twitter.com/'
+                                + word[1:]
+                                + '>`_')
                     else:
-                        word = word.replace(word, '`' + word[:-1] + ' <https://twitter.com/' + word[1:-1] + '>`_' + word[-1])
+                        word = word.replace(
+                                word, '`'
+                                + word[:-1]
+                                + ' <https://twitter.com/'
+                                + word[1:-1]
+                                + '>`_'
+                                + word[-1])
                 if word[0] == "#":
                     if word[-1].isalnum():
-                        word = word.replace(word, '`' + word + ' <https://twitter.com/hashtag/' + word[1:] + '>`_')
+                        word = word.replace(
+                                word, '`'
+                                + word
+                                + ' <https://twitter.com/hashtag/'
+                                + word[1:]
+                                + '>`_')
                     else:
-                        word = word.replace(word, '`' + word[:-1] + ' <https://twitter.com/hashtag/' + word[1:-1] + '>`_' + word[-1])
+                        word = word.replace(
+                                word, '`'
+                                + word[:-1]
+                                + ' <https://twitter.com/hashtag/'
+                                + word[1:-1]
+                                + '>`_' + word[-1])
                 text_2.append(word)
             text = ' '.join(text_2)
             data += ":summary: " + summary + "\n"
@@ -107,19 +161,40 @@ def tweet2rest(tweets_json):
                 stat("./content/SocialNetworks")
             except:
                 mkdir("./content/SocialNetworks")
-            f = open("./content/SocialNetworks/tweet_" + tweet['id_str'] + ".rst", "w", encoding="UTF-8")
+            f = open(
+                    "./content/SocialNetworks/tweet_"
+                    + tweet['id_str']
+                    + ".rst", "w", encoding="UTF-8")
             f.write(data)
             logging.debug("Tweet number " + tweet['id_str'] + " saved !")
             f.close()
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tweets -> ReST')
-    parser.add_argument('-D', '--debug', action="store_true")
-    parser.add_argument('token', help="Please provide a Token (See https://apps.twitter.com/)")
-    parser.add_argument('token_key', help="Please provide a Token Secret (See https://apps.twitter.com/)")
-    parser.add_argument('con_secret', help="Please provide a Consumer Key (See https://apps.twitter.com/)")
-    parser.add_argument('con_secret_key', help="Please provide a Consumer Secret (See https://apps.twitter.com/)")
-    parser.add_argument('twitter_name', help="Please provide your Twitter screen name")
+    parser.add_argument(
+            '-D',
+            '--debug',
+            action="store_true")
+    parser.add_argument(
+            'token',
+            help="""Please provide a Token
+            (See https://apps.twitter.com/)""")
+    parser.add_argument(
+            'token_key',
+            help="""Please provide a Token Secret
+            (See https://apps.twitter.com/)""")
+    parser.add_argument(
+            'con_secret',
+            help="""Please provide a Consumer Key
+            (See https://apps.twitter.com/)""")
+    parser.add_argument(
+            'con_secret_key',
+            help="""Please provide a Consumer Secret
+            (See https://apps.twitter.com/)""")
+    parser.add_argument(
+            'twitter_name',
+            help="""Please provide your Twitter screen name""")
     args = parser.parse_args()
     settings = vars(args)
     if args.debug:
